cc1aac74ec172b14a6232cb3706511a6fa6903dc
rabiacete <rabiacete6@gmail.com>
2025-09-09 13:36:31 +0300

    Initial Commit

---
+++ FlashCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashCard
+//
+//  Created by Rabia Çete on 9.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
